90f343eb8022d8f3bdb745b6aee543f0b186c979
Muhammad Rivky Ananda <rivkyananda33@gmail.com>
2025-12-26 11:22:23 +0700

    Update init_admin.py

--- app/utils/init_admin.py
+++ app/utils/init_admin.py
@@ -1,15 +1,22 @@
-def create_default_admin():
-    from app.models.user_model import User
-    from app import db
+from sqlalchemy.exc import ProgrammingError
+from app import db
 
-    admin = User.query.filter_by(username="admin").first()
-    if not admin:
-        admin = User(
-            nik_user="0000000000000000",
-            nama_user="Administrator",
-            username="admin",
-            role="admin"
-        )
-        admin.password = "admin123"
-        db.session.add(admin)
-        db.session.commit()
+def create_default_admin():
+    try:
+        from app.models.user_model import User
+
+        admin = User.query.filter_by(username="admin").first()
+        if not admin:
+            admin = User(
+                nik_user="0000000000000000",
+                nama_user="Administrator",
+                username="admin",
+                role="admin"
+            )
+            admin.password = "admin123"
+            db.session.add(admin)
+            db.session.commit()
+
+    except ProgrammingError:
+        # Tabel belum ada (migration belum dijalankan)
+        db.session.rollback()
