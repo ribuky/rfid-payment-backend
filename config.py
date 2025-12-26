3eb2e36bebc3846615c3314b7f48f2185f8976ec
Muhammad Rivky Ananda <rivkyananda33@gmail.com>
2025-12-26 10:54:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,10 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@"
+        f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@"
         f"{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB')}"
     )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", SECRET_KEY)
